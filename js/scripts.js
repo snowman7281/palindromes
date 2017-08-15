e6090b33df6d27e42b217985bba95ff5b1ed86a0
Kristen Kulha and snow vilay <kristen@>
2017-08-15 13:43:33 -0700

    add template for user to enter a word to see if the word is a palindrome, not yet functional

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#palindromeForm").submit(function(event){
+    event.preventDefault();
+
+    var wordInput = $("#word").val();
+
+    function reverseString(str){
+      var splitString = str.split("");
+      var reverseArray = splitString.reverse();
+      var joinArray = reverseArray.join("");
+      return joinArray;
+    });
+
+    if(wordInput == reverseString(wordInput)){
+      
+    }else{
+
+    }
+
+  });
+});
